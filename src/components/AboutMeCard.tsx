90de824b642fbbb57cb6fc49f65aa74dda9cfcbf
Chris Doyle <sean.chris.doyle@gmail.com>
2021-09-03 16:47:14 -0400

    updating wording in about me section

--- src/components/AboutMeCard.tsx
+++ src/components/AboutMeCard.tsx
@@ -10,7 +10,7 @@ const AboutMeCard:FC = () => {
       <Box maxW={{ base: '75vw', sm: '75vw', md: '30vw', lg: '30vw' }} ml="20" color="whiteAlpha.900" mt="2" fontSize="lg" fontWeight="light" display="flex" flexDirection="column">
         <Text fontWeight="semibold" fontSize="2xl">A little about me...</Text>
         <List mt="2">
-          <ListItem><ListIcon as={MdChevronRight}/>I&apos;ve developed projects at different scales, from company-wide systems to smale-scale access tracking systems on bare metal</ListItem>
+          <ListItem><ListIcon as={MdChevronRight}/>I&apos;ve developed projects at different scales, from company-wide systems to small-scale access tracking systems on bare metal</ListItem>
           <ListItem><ListIcon as={MdChevronRight}/>Moving forward, I&apos;m most intrigued by exploring the NodeJS stack as micro-services and micro frontends</ListItem>
         </List>
         <Text color="whiteAlpha.900" mt="2" flexGrow={1} display="flex" alignItems="flex-end">Thanks for taking the time to take a look at my portfolio!</Text>
